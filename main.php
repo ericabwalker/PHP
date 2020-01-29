c088c8e3b944953a2a7954c982f6ceb1f2a85dfe
Erica Walker <47954578+ericabwalker@users.noreply.github.com>
2020-01-29 16:59:49 -0500

    Creating result integer variable

--- main.php
+++ main.php
@@ -3,4 +3,6 @@ require 'constants.php';
 
 $array = array("first_name"=>"Erica", "nickname"=>"America", "language"=>"PHP", "int_one"=>1, "int_two"=>2);
 
+$result = $array['int_two'] * $array['int_one'];
+
 ?>
