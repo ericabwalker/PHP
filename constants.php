73616d4f3df50f22ab09fab2536158159d9711d3
Erica Walker <47954578+ericabwalker@users.noreply.github.com>
2020-01-29 16:25:38 -0500

    Correcting int constants

--- constants.php
+++ constants.php
@@ -2,6 +2,6 @@
 define("first_name", "Erica");
 define("nickname", "America");
 define("language", "PHP");
-define("int_one", "1");
-define("int_two", "2");
+define("int_one", 1);
+define("int_two", 2);
 ?>
