9a39d4c98975c4cb1de39efef5f5adefe4e50505
Erica Walker <47954578+ericabwalker@users.noreply.github.com>
2020-01-30 15:23:03 -0500

    foreach loop to iterate over array without if-else statement

--- loop-control.php
+++ loop-control.php
@@ -1,3 +1,19 @@
-<?php 
+<?php
 $numbers = range(0, 100, 3);
+
+foreach ($numbers as $number) {
+    switch ($number) {
+        case 3:
+            echo "Three ";
+            break;
+        case 9:
+            echo "Nine Nine Nine ";
+            break;
+        case 15:
+            echo "Fifteen Fifteen Fifteen Fifteen Fifteen ";
+            break;
+        default:
+            echo $number . " ";
+    }
+}
 ?>
