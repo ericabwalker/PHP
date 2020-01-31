ced31a6ff094a653d6fdea55e2c9446f80afe7b0
Erica Walker <47954578+ericabwalker@users.noreply.github.com>
2020-01-31 09:57:40 -0500

    for loop to iterate over array with if-elseif-else

--- loop-control.php
+++ loop-control.php
@@ -2,17 +2,16 @@
 $numbers = range(0, 100, 3);
 
 for ($i = 0; $i < count($numbers); $i++) {
-    switch ($numbers[$i]) {
-        case 3:
-            echo "Three ";
-            break;
-        case 9:
-            echo "Nine Nine Nine ";
-            break;
-        case 15:
-            echo "Fifteen Fifteen Fifteen Fifteen Fifteen ";
-            break;
-        default:
-            echo $numbers[$i] . " ";
-    }
+    if ($numbers[$i] === reset($numbers)) {
+        echo "First number | ";
+    } elseif ($numbers[$i] % 10 == 0) {
+        echo "$numbers[$i] is a round number | ";
+    } elseif ($numbers[$i] % 7 == 0) {
+        $result = $numbers[$i] / 7;
+        echo "Sevens are lucky, this number has $result | ";
+    } elseif ($numbers[$i] === end($numbers)) {
+        echo "Last number ";
+    } else {
+        echo $numbers[$i] . " | ";
+    } 
 }
