5afb17260d8730c87b51f983b27f2c3a41c6e0bb
Erica Walker <47954578+ericabwalker@users.noreply.github.com>
2020-01-31 11:23:49 -0500

    Create four math functions with two parameters

---
+++ functions.php
@@ -0,0 +1,18 @@
+<?php 
+
+function add(int $a, int $b) {
+    return $a + $b;
+}
+
+function subtract(int $a, int $b) {
+    return $a - $b;
+}
+
+function multiply(int $a, int $b) {
+    return $a * $b;
+}
+
+function divide(int $a, int $b) {
+    return $a / $b;
+}
+?>
