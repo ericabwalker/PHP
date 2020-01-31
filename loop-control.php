7a5bceefadf3c82c0117c628ca38cff7dc312a23
Erica Walker <47954578+ericabwalker@users.noreply.github.com>
2020-01-31 09:49:26 -0500

    for loop to iterate over array without if-else statement

--- loop-control.php
+++ loop-control.php
@@ -1,18 +1,18 @@
 <?php
 $numbers = range(0, 100, 3);
 
-foreach ($numbers as $number) {
-    if ($number === reset($numbers)) {
-        echo "First number | ";
-    } elseif ($number % 10 == 0) {
-        echo "$number is a round number | ";
-    } elseif ($number % 7 == 0) {
-        $result = $number / 7;
-        echo "Sevens are lucky, this number has $result | ";
-    } elseif ($number === end($numbers)) {
-        echo "Last number ";
-    } else {
-        echo $number . " | ";
-    }   
+for ($i = 0; $i < count($numbers); $i++) {
+    switch ($numbers[$i]) {
+        case 3:
+            echo "Three ";
+            break;
+        case 9:
+            echo "Nine Nine Nine ";
+            break;
+        case 15:
+            echo "Fifteen Fifteen Fifteen Fifteen Fifteen ";
+            break;
+        default:
+            echo $numbers[$i] . " ";
+    }
 }
-?>
