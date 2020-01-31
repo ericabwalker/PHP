ebfd57e33b8581cad17851551efb2e13405b0dcf
Erica Walker <47954578+ericabwalker@users.noreply.github.com>
2020-01-31 11:25:48 -0500

    Print result of functions, passing args by value and reference

--- functions.php
+++ functions.php
@@ -15,4 +15,25 @@ function multiply(int $a, int $b) {
 function divide(int $a, int $b) {
     return $a / $b;
 }
+
+echo add(10, 60) . " ";
+echo subtract(100, 80) . " ";
+echo multiply(5, 10) . " ";
+echo divide(80, 2) . " ";
+
+$a = 10;
+$b = 60;
+echo add($a, $b) . " ";
+
+$a = 100;
+$b = 80;
+echo subtract($a, $b) . " ";
+
+$a = 5;
+$b = 10;
+echo multiply($a, $b) . " ";
+
+$a = 80;
+$b = 2;
+echo divide($a, $b) . " ";
 ?>
