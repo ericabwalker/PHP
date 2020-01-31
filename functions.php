9f7bea9deb967e9e085bb30aef865f3c7665891a
Erica Walker <47954578+ericabwalker@users.noreply.github.com>
2020-01-31 18:25:40 -0500

    compare function

--- functions.php
+++ functions.php
@@ -36,4 +36,24 @@ echo multiply($a, $b) . " ";
 $a = 80;
 $b = 2;
 echo divide($a, $b) . " ";
+
+function compare($c, $d, $e = false) {
+    if ($c == $d && $e == false) {
+        echo "$c and $d are equal. ";
+    } elseif ($c == $d && $e == true) {
+        if ($c === $d) {
+            echo "$c and $d are equal and equal in datatype. ";
+        } else {
+            echo "$c and $d are equal but not equal in datatype. ";
+        }
+    } else {
+        echo "$c and $d are not equal. ";
+    }
+}
+
+echo compare(4, "4");
+echo compare (5, "5", true);
+echo compare (4, 4.0);
+echo compare (5, 5.0, true);
+
 ?>
