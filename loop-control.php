b24132508e5d37218eac4b800035fc4ed8d3babc
Erica Walker <47954578+ericabwalker@users.noreply.github.com>
2020-01-31 11:07:43 -0500

    do-while loop to iterate over the array without if-else statement

--- loop-control.php
+++ loop-control.php
@@ -2,23 +2,23 @@
 $numbers = range(0, 100, 3);
 
 $i = 0;
-while ($i < count($numbers)) {
-    if ($numbers[$i] === reset($numbers)) {
-        echo "First number | ";
-        $i++;
-    } elseif ($numbers[$i] % 10 == 0) {
-        echo "$numbers[$i] is a round number | ";
-        $i++;
-    } elseif ($numbers[$i] % 7 == 0) {
-        $result = $numbers[$i] / 7;
-        echo "Sevens are lucky, this number has $result | ";
-        $i++;
-    } elseif ($numbers[$i] === end($numbers)) {
-        echo "Last number ";
-        $i++;
-    } else {
-        echo $numbers[$i] . " | ";
-        $i++;
-    } 
-}
+do {
+    switch ($numbers[$i]) {
+        case 3:
+            echo "Three ";
+            $i++;
+            break;
+        case 9:
+            echo "Nine Nine Nine ";
+            $i++;
+            break;
+        case 15:
+            echo "Fifteen Fifteen Fifteen Fifteen Fifteen ";
+            $i++;
+            break;
+        default:
+            echo $numbers[$i] . " ";
+            $i++;
+    }
+} while ($i < count($numbers));
 ?>
