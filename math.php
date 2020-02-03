53cccca0a1e695024dd5f25f6e3f51493f53d17c
Erica Walker <47954578+ericabwalker@users.noreply.github.com>
2020-02-03 09:52:33 -0500

    Creation of math.php file and Math class with four math functions

---
+++ math.php
@@ -0,0 +1,20 @@
+<?php 
+class Math {
+    public function add(int $a, int $b) {
+        return $a + $b;
+    }
+    
+    public function subtract(int $a, int $b) {
+        return $a - $b;
+    }
+    
+    public function multiply(int $a, int $b) {
+        return $a * $b;
+    }
+    
+    public function divide(int $a, int $b) {
+        return $a / $b;
+    }
+}
+
+?>
