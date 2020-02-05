b79da36a039ef8dcab1f75a0681e380a9c5f45c8
Erica Walker <47954578+ericabwalker@users.noreply.github.com>
2020-02-05 14:08:58 -0500

    Inheritance hierarchy composed of six classes

---
+++ advanced/inheritance_hierarchy.php
@@ -0,0 +1,31 @@
+<?php 
+
+class Computer {
+
+}
+
+class Workstation extends Computer {
+
+}
+
+class Mac extends Workstation {
+
+}
+
+class PC extends Workstation {
+
+}
+
+class Server extends Computer {
+
+}
+
+class WebServer extends Server {
+
+}
+
+class DatabaseServer extends Server {
+
+}
+
+?>
