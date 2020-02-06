b2bbc290bad54dd3fc1e6276cbe3b0e2e9a24960
Erica Walker <47954578+ericabwalker@users.noreply.github.com>
2020-02-06 14:45:33 -0500

    Adding public/private/protected properties and creating main.php file

--- advanced/inheritance_hierarchy.php
+++ advanced/inheritance_hierarchy.php
@@ -1,31 +1,52 @@
 <?php 
 
 class Computer {
-
+    public $model;
+    private $serial_number; 
+    public $storage_capacity;
+    protected $nickname;
+    private $users;
+    public $amt_RAM; 
+    public $manufacture_yr; 
 }
 
 class Workstation extends Computer {
-
+    public $LAN_member; 
+    private $LAN_address;
+    public $graphics_cards; 
+    public $operating_sys; 
 }
 
 class Mac extends Workstation {
-
+    public $power_connection; 
+    protected $has_applecare; 
+    public $is_laptop; 
+    public $has_touchID; 
+    public $is_justin_long; 
 }
 
 class PC extends Workstation {
-
+    public $manufacturer; 
+    public $is_john_hodgman; 
 }
 
 class Server extends Computer {
-
+    public $services; 
+    protected $clients; 
+    private $ssh_keys; 
+    protected $location; 
+    protected $last_request;
 }
 
 class WebServer extends Server {
-
+    public $LAN_member; 
+    private $LAN_address; 
+    protected $load_limit;
 }
 
 class DatabaseServer extends Server {
-
+    public $query_language;
+    public $database_application;
 }
 
 ?>
---
+++ advanced/main.php
@@ -0,0 +1,9 @@
+<?php 
+include 'inheritance_hierarchy.php';
+
+
+
+
+
+
+?>
