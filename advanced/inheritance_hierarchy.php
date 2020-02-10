5eb7b8a697a44caa5a6416e2f79c42f89b4c6327
Erica Walker <47954578+ericabwalker@users.noreply.github.com>
2020-02-10 09:54:57 -0500

    Adding public/private/protected functions

--- advanced/inheritance_hierarchy.php
+++ advanced/inheritance_hierarchy.php
@@ -8,6 +8,44 @@ class Computer {
     private $users;
     public $amt_RAM; 
     public $manufacture_yr; 
+
+    public function get_model($model) {
+        echo $model;
+    }
+
+    private function get_serial_number($serial_number) {
+        echo $serial_number;
+    }
+
+    public function print_serial($serial_number) {
+        $this->get_serial_number($serial_number);
+    }
+
+    public function get_storage_capacity($storage_capacity) {
+        echo $storage_capacity;
+    }
+
+    protected function get_nickname($nickname) {
+        echo $nickname;
+    }
+
+    public function print_nickname($nickname) {
+        $this->get_nickname($nickname);
+    }
+
+    public function get_users($users) {
+        foreach ($users as $user) {
+            echo $user . " ";
+        }
+    }
+
+    public function get_RAM($amt_RAM) {
+        echo $amt_RAM;
+    }
+
+    public function get_year($manufacture_yr) {
+        echo $manufacture_yr;
+    }
 }
 
 class Workstation extends Computer {
@@ -15,6 +53,26 @@ class Workstation extends Computer {
     private $LAN_address;
     public $graphics_cards; 
     public $operating_sys; 
+
+    public function is_LAN_member($LAN_member) {
+        echo $LAN_member;
+    }
+
+    private function get_LAN_address($LAN_address) {
+        echo $LAN_address;
+    }
+
+    public function print_lan_address($LAN_address) {
+        $this->get_LAN_address($LAN_address);
+    }
+
+    public function num_graphics_cards($graphics_cards) {
+        echo $graphics_cards;
+    }
+
+    public function get_operating_system($operating_sys) {
+        echo $operating_sys;
+    }
 }
 
 class Mac extends Workstation {
@@ -23,11 +81,43 @@ class Mac extends Workstation {
     public $is_laptop; 
     public $has_touchID; 
     public $is_justin_long; 
+
+    public function get_power_connection($power_connection) {
+        echo $power_connection;
+    }
+
+    protected function has_applecare($has_applecare) {
+        echo $has_applecare;
+    }
+
+    public function print_applecare($has_applecare) {
+        $this->has_applecare($has_applecare);
+    }
+
+    public function is_laptop($is_laptop) {
+        echo $is_laptop;
+    }
+
+    public function has_touchID($has_touchID) {
+        echo $has_touchID;
+    }
+
+    public function is_justin_long($is_justin_long) {
+        echo $is_justin_long;
+    }
 }
 
 class PC extends Workstation {
     public $manufacturer; 
     public $is_john_hodgman; 
+
+    public function get_manufacturer($manufacturer) {
+        echo $manufacturer;
+    }
+
+    public function is_john_hodgman($is_john_hodgman) {
+        echo $is_john_hodgman;
+    }
 }
 
 class Server extends Computer {
@@ -36,17 +126,85 @@ class Server extends Computer {
     private $ssh_keys; 
     protected $location; 
     protected $last_request;
+
+    public function get_services($services) {
+        foreach ($services as $service) {
+            echo $service . " ";
+        }
+    }
+
+    protected function get_clients($clients) {
+        foreach ($clients as $client) {
+            echo $client . " ";
+        }
+    }
+
+    public function print_clients($clients) {
+        $this->get_clients($clients);
+    }
+
+    private function get_ssh($ssh_keys) {
+        foreach ($ssh_keys as $key) {
+            echo $key . " ";
+        }
+    }
+
+    public function print_ssh($ssh_keys) {
+        $this->get_ssh($ssh_keys);
+    }
+
+    protected function get_location($location) {
+        echo $location;
+    }
+
+    public function print_location($location) {
+        $this->get_location($location);
+    }
+
+    protected function get_last_request($last_request) {
+        echo $last_request;
+    }
+
+    public function print_last_request($last_request) {
+        $this->get_last_request($last_request);
+    }
 }
 
 class WebServer extends Server {
     public $LAN_member; 
     private $LAN_address; 
     protected $load_limit;
+
+    public function is_LAN_member($LAN_member) {
+        echo $LAN_member;
+    }
+
+    private function get_LAN_address($LAN_address) {
+        echo $LAN_address;
+    }
+
+    public function print_lan_address($LAN_address) {
+        $this->get_LAN_address($LAN_address);
+    }
+
+    protected function get_load_limit($load_limit) {
+        echo $load_limit;
+    }
+
+    public function print_load_limit($load_limit) {
+        $this->get_load_limit($load_limit);
+    }
 }
 
 class DatabaseServer extends Server {
     public $query_language;
     public $database_application;
-}
 
-?>
+    public function get_query_language($query_language) {
+        echo $query_language;
+    }
+
+    public function get_database_application($database_application) {
+        echo $database_application;
+    }
+}
--- advanced/main.php
+++ advanced/main.php
@@ -1,9 +1,64 @@
 <?php 
 include 'inheritance_hierarchy.php';
 
+$computer1 = new Computer();
+$computer1->get_model("test ");
+$computer1->print_serial("123456789abc ");
+$computer1->get_storage_capacity("156GB ");
+$computer1->print_nickname("bob ");
+$computer1->get_users(array('Sarah ', 'James ', 'Bill '));
+$computer1->get_RAM("3289328 ");
+$computer1->get_year("2019 ");
+
+
+$workstation1 = new Workstation();
+$workstation1->is_LAN_member(true);
+echo " ";
+$workstation1->print_lan_address("14028.2390.29 ");
+$workstation1->num_graphics_cards(4);
+echo " ";
+$workstation1->get_operating_system("Catalina ");
+$workstation1->get_model("a model computer ");
+
+
+$macbook1 = new Mac();
+$macbook1->get_power_connection("Lightning");
+$macbook1->print_applecare(false);
+echo " ";
+$macbook1->is_laptop(true);
+echo " ";
+$macbook1->has_touchID(false);
+echo " ";
+$macbook1->is_justin_long(true);
+echo " ";
+
+
+$pc1 = new PC();
+$pc1->get_manufacturer("Dell ");
+$pc1->is_john_hodgman(true);
+echo " ";
+$pc1->num_graphics_cards(3);
+echo " ";
+
+
+$server1 = new Server();
+$server1->get_services(array("test", "service ")); 
+$server1->print_clients(array("Sally", " Jesse ")); 
+$server1->print_ssh(array("ssh", "key "));
+$server1->print_location("California ");
+$server1->print_last_request("GET ");
 
 
 
+$webserver1 = new WebServer();
+$webserver1->is_LAN_member(true);
+echo " ";
+$webserver1->print_lan_address("123.45.53 ");
+$webserver1->print_load_limit("12GB ");
 
+
+$databaseserver1 = new DatabaseServer();
+$databaseserver1->get_query_language(" MySQL ");
+$databaseserver1->get_database_application("Sabre ");
 
 ?>
